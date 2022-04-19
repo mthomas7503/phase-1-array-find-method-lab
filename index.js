56c8ee69a4c095d9e2d1fb6d0e815dff05a18864
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-04-19 17:57:48 -0400

    initial commit with passed tests

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(array) {
+if (array.find(x => x.result === 'W') == undefined) {return undefined}
+else if (array.find(x => x.result === 'W').result === 'W') {return array.find(x => x.result === 'W').year}
+}
